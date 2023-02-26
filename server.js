28d18d63bed64218fd1e8a822966589103decc6d
jestershit32 <max.vedenev2@mail.ru>
2023-02-26 23:53:20 +0600

    доп get

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const Post = require('./models/post');
 const User = require('./models/user');
+const { URL } = require('../GLOBAL');
+
 
 
 const PORT = 3000;
-const URL = "mongodb+srv://admin:admin1337@hranilishe.qimdddk.mongodb.net/diplom?retryWrites=true&w=majority";
+
 
 
 
@@ -21,14 +23,29 @@ const handleError = (res, message) => {
 }
 
 const app = express();
+app.use(express.json());
 
 
 
+app.post('/create-post', cors(), (req, res) => {
+    const post = new Post(req.body);
+    console.log(req);
+    post
+        .save()
+        .then((result) => {
+            res
+                .status(200)
+                .json(result);
+        })
+        .catch((err) => { handleError(res, err) })
+})
 
 app.get("/post", cors(), (req, res) => {
     Post
         .find()
-        .sort({ title: 1 })
+        // .skip(2)
+        // .limit(4)
+        .sort({ title: -1 })
         .then((post) => {
             res
                 .status(200)
@@ -36,6 +53,7 @@ app.get("/post", cors(), (req, res) => {
         })
         .catch(() => handleError(res, "чето не так с постами"))
 })
+
 
 
 
@@ -84,13 +102,24 @@ app.get("/profile/:id", cors(), (req, res) => {
 
 
 
+
+
+
 app.use(cors());
 
+
+// Запуск подключения к базе данных
 mongoose
+    .set('strictQuery', true)
     .connect(URL)
     .then(() => console.log("connection"))
     .catch((err) => console.log("db connect err " + err));
 
+
+
+
+
+// создание сервера
 app.listen(PORT, (err) => {
     err ? console.log(err) : console.log("listening on port " + PORT)
 })
